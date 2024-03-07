1ea3e8c8fb3fc2fd912430cb4739662a026c8557
Patryck Silva <patrycksilva70@gmail.com>
2024-03-07 00:22:18 -0300

    feat: configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        dark: '#141415',
+        light: '#dfe6e9'
+      }
+    }
   },
   plugins: []
 }
